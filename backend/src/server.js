1ea59ca051520ac74b0bdbb244be428368e13eaa
Daniel Herênio <santosherenio76@gmail.com>
2025-01-28 18:25:37 -0300

    alterei o caminho

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,27 +1,25 @@
-const { WebSocketServer } = require('ws');
-const dotenv = require('dotenv');
+const { WebSocketServer } = require('ws')
+const dotenv = require('dotenv')
 
-dotenv.config();
 
-const wss = new WebSocketServer({ port: process.env.Port || 8080 });
+
+dotenv.config()
+
+const wss = new WebSocketServer({ port: process.env.Port || 8080 })
 
 wss.on('connection', (ws) => {
-    ws.on("error", console.error);
+    ws.on("error", console.error)
 
     ws.on("message", (data) => {
-        console.log(data.toString());
+        console.log(data.toString()) 
 
-        // Envia a mensagem para todos os outros clientes conectados
+        
         wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === client.OPEN) {
-                client.send(data.toString());
-            }
-        });
-    });
+            
+            client.send(data.toString())
+        })
+    })
 
-    console.log("Cliente conectado");
+    console.log("Client connected")
+})
 
-    ws.on('close', () => {
-        console.log("Cliente desconectado");
-    });
-});
